2e6c0f09933cbf68ba7609a5df17e1eacfe20a2f
DinoStarBros <lmatthewalven@gmail.com>
2025-09-11 19:52:14 +0800

    Refactor skill button visuals and logic
    Updated skill button sizing and positioning in .tscn files for consistency. Refactored skill prerequisite line drawing logic and color handling in skill_button.gd. Removed debug print statements and improved type usage in skill_tree.gd. Adjusted skill button placement in skill_tree.tscn.

--- PauseUI/skillTreeStuff/skill_button/skill_button.gd
+++ PauseUI/skillTreeStuff/skill_button/skill_button.gd
@@ -14,21 +14,29 @@ var level : int = 0:
 		level = value
 		label.text = str(level) + "/" + str(MAX_LEVEL)
 var skill_tree : SkillTree
+var ndex : int = 0
 
 func _ready() -> void:
 	skill_tree = get_parent()
 	
 	pressed.connect(_pressed)
+	
 	label.text = str(level) + "/" + str(MAX_LEVEL)
-	if get_parent() is SkillButton:
+	#if get_parent() is SkillButton:
+	for prereq_skill in prerequisite_skills:
 		line_2d.add_point(global_position + size/2)
-		line_2d.add_point(get_parent().global_position + size/2)
+		line_2d.add_point(prereq_skill.global_position + size/2)
 		
-		line_2d.points[0].y += size.y / 2
-		line_2d.points[1].y += size.y / 2
+		#ndex += 1
+		#line_2d.points[ndex-1].y += size.y / 2
+		#line_2d.points[ndex].y += size.y / 2
 
 var prereq_skl_lvls : Array
 func _pressed() -> void:
+	if level < MAX_LEVEL:
+		skill_up()
+
+func skill_up() -> void:
 	prereq_skl_lvls.clear()
 	if prerequisite_skills.size() != 0:
 		
@@ -36,10 +44,11 @@ func _pressed() -> void:
 			prereq_skl_lvls.append(skill.level)
 		
 		if prereq_skl_lvls.count(1) == prereq_skl_lvls.size():
+			
 			level = min(level+1, MAX_LEVEL)
 			panel.show_behind_parent = true
 			
-			line_2d.default_color = Color.SKY_BLUE
+			line_2d.default_color = Color.GREEN
 		
 		skill_tree.ability_pressed()
 	else:
@@ -47,6 +56,6 @@ func _pressed() -> void:
 		level = min(level+1, MAX_LEVEL)
 		panel.show_behind_parent = true
 		
-		line_2d.default_color = Color.SKY_BLUE
+		#line_2d.default_color = Color.GREEN
 		
 		skill_tree.ability_pressed()
--- PauseUI/skillTreeStuff/skill_tree.gd
+++ PauseUI/skillTreeStuff/skill_tree.gd
@@ -6,8 +6,6 @@ var skills_levels : Array
 
 func _ready() -> void:
 	update_skill_arrays()
-	print(skills_in_tree)
-	print(skills_levels)
 
 func update_skill_arrays() -> void:
 	skills_in_tree.clear()
@@ -16,9 +14,12 @@ func update_skill_arrays() -> void:
 		if child is SkillButton:
 			skills_in_tree.append(child)
 			skills_levels.append(child.level)
+	
+	#print(skills_in_tree)
+	#print(skills_levels)
 
 func set_skills_values() -> void:
-	for n in skills_in_tree:
+	for skill : SkillButton in skills_in_tree:
 		pass
 
 func ability_pressed() -> void:
